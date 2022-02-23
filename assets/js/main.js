a18ea8bab4cf775500c08fe14128a241d8d5b3cb
Waldemar Teichreb <waldi.teichreb@gmail.com>
2022-02-23 20:09:16 +0100

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,25 @@ const enterButton = document.querySelector('#enter');
 const userInput = document.querySelector('#userinput');
 const unorderedList = document.querySelector('ul');
 
-enterButton.addEventListener('click', () => {
-  createList();
+console.log(userInput.value);
+
+enterButton.addEventListener('click', createListItem = () => {
+  createListItem();
 });
 
 document.body.addEventListener('keydown', (e) => {
   if(e.keyCode == 13) {
-    createList();
+    createListItem();
   }
 });
 
-createList = () => {
+createListItem = () => {
   let newItem = document.createElement('li');
   newItem.textContent = userInput.value;
+
+  if(userInput.value == 0) {
+    return;
+  }
+  
   unorderedList.appendChild(newItem);
 }
