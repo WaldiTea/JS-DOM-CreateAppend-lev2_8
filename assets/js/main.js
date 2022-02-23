0b157cc84cb0b428d2a6a1ec5ee6dc5aa69cd1b1
Waldemar Teichreb <waldi.teichreb@gmail.com>
2022-02-23 19:10:54 +0100

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,17 @@ const userInput = document.querySelector('#userinput');
 const unorderedList = document.querySelector('ul');
 
 enterButton.addEventListener('click', () => {
+  createList();
+});
+
+document.body.addEventListener('keydown', (e) => {
+  if(e.keyCode == 13) {
+    createList();
+  }
+});
+
+createList = () => {
   let newItem = document.createElement('li');
   newItem.textContent = userInput.value;
   unorderedList.appendChild(newItem);
-});
+}
